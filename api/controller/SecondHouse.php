b0210c5f12373b1f1df1cae475cdca04fd79dba6
xiaobin <283817727@qq.com>
2020-05-20 21:42:39 +0800

    推荐房源详情

--- api/controller/SecondHouse.php
+++ api/controller/SecondHouse.php
@@ -4,9 +4,14 @@ namespace app\api\controller;
 
 use app\home\service\IndexServer;
 use app\home\service\SecondServer;
+use app\server;
 use app\tools\ApiResult;
 use think\Controller;
 
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Methods:GET, POST, OPTIONS, DELETE");
+header("Access-Control-Allow-Headers:DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type, Accept-Language, Origin, Accept-Encoding");
+//……
 class SecondHouse extends Controller
 {
     use ApiResult;
@@ -58,6 +63,32 @@ class SecondHouse extends Controller
         }
     }
     
-
-
+    
+    /**
+     * @description 推荐房源详情
+     * @param $id 房源ID
+     * @return \think\response\Json
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     * @auther xiaobin
+     */
+    public function houseDetail($id)
+    {
+        $house = new server();
+        $houseRes = $house->second_model($id);
+        $houseRes['toilet'] = getLinkMenuName(29,$houseRes['toilet']);
+        $houseRes['acreage'] = $houseRes['acreage'].config('filter.acreage_unit');
+        $houseRes['estate'] = $house->estate($houseRes['estate_id']);
+        $houseRes['orientations'] = getLinkMenuName(4,$houseRes['orientations']);
+        $houseRes['user'] = $house->user_info($id,$houseRes['broker_id']);
+        $houseRes['user'] =  $houseRes['user'] && $houseRes['user']['history_complate'] ? $houseRes['user']['history_complate'] : 0;
+        $houseRes['user_info'] = $house->user($id,$houseRes['broker_id']);
+        $houseRes['user_info'] = $houseRes['user_info'] && $houseRes['user_info']['lxtel_zhuan'] ? $houseRes['user_info']['lxtel_zhuan'] : '';
+        $houseRes['pinglun'] = model('user')->where('id',$houseRes['broker_id'])->find();//客服
+        $user = login_user();
+        $houseRes['is_guanzhu'] = $house->is_guanzhu($houseRes['id'],$user['id']);
+        $houseRes['user_id'] = $user['id'];
+        return $this->success_o($houseRes);
+    }
 }
