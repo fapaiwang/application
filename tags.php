e8407b1ca43dbab0329a6e12f6624a3a4569cd35
al <>
2020-05-28 22:14:29 +0800

    接口跨域

--- tags.php
+++ tags.php
@@ -5,7 +5,7 @@ return [
     // 应用初始化
     'app_init'     => [],
     // 应用开始
-    'app_begin'    => [],
+    'app_begin'    => ['app\\common\\behavior\\CronRun'],
     // 模块初始化
     'module_init'  => [],
     // 操作开始执行
@@ -15,5 +15,5 @@ return [
     // 日志写入
     'log_write'    => [],
     // 应用结束
-    'app_end'      => [],
+    'app_end'      => ['app\\common\\behavior\\CronRun'],
 ];
