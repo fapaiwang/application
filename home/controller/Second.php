c49c750edf63f3fbda96c2cedbb997e4c519eb0a
al <>
2020-06-16 15:21:06 +0800

    税费bug

--- home/controller/Second.php
+++ home/controller/Second.php
@@ -186,7 +186,7 @@ class Second extends HomeBase{
             $this->assign('fenxiang_img', $fenxiang_img);
             $userInfo = $this->getUserInfo();
             $user = model('user')->field('share_img')->where('id',$userInfo["id"])->find();
-            $userInfo["qr_code"] = $user->share_img;
+            $userInfo["qr_code"] = $user->share_img ?? "";
             $this->assign('login_user',$userInfo);
         }else{
             return $this->fetch('public/404');
