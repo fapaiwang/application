e44cae4d1ea0119984276c58efc6b64b7ebcdc96
al <>
2020-06-18 15:15:23 +0800

    周分享

--- home/controller/Login.php
+++ home/controller/Login.php
@@ -244,4 +244,55 @@ class Login extends HomeBase
         $this->assign('agreement',$agreement);
         return $this->fetch();
     }
+    /**
+     * 每周公司分享
+     * @param mixed
+     * @return mixed|string
+     * @author: al
+     */
+    public function company_week_recommended(){
+        $IndexServer = new IndexServer();
+        //时间
+        $time["year"] = date("Y");
+        $time['s_time']=date('m/d', (time() - ((date('w') == 0 ? 7 : date('w')) - 1) * 24 * 3600));
+        $time['e_time']=date('m/d', (time() + (7 - (date('w') == 0 ? 7 : date('w'))) * 24 * 3600));
+
+        //查询数据
+        $recommended =model("a_zhoutuijian")->select();
+        $res = Db::query('select max(pid) as num from fang_a_zhoutuijian');
+        if (empty($res[0]["num"])){
+            return "统计房源城市pid错误";
+        }
+        //颜色
+        $color["background"] ="#b89686";
+        $color["city"] ="#EDCBBB";
+        $color["one_line"] ="#F2E2DA";
+        $color["double_row"] ="#FFFFFF";
+        //背景图
+        $back_img = $IndexServer->get_home_banner_x(28,1);
+//dd($back_img->setting["fileurl"]);
+        $arr = array_range(1, $res[0]["num"], 1);
+        foreach ($arr as $key=>$val){
+            $num =0;
+            $last_num = 0;
+            foreach ($recommended as $k=>$v){
+                if (!empty($v->city)){
+                    $last_num = $k;
+                }
+                if ($val == $v->pid){
+                    $num++;
+                    $recommended[$k]["num"] =$num;
+                    $recommended[$last_num]["num"] =$num;
+                }
+            }
+        }
+        $this->assign('time',$time);
+        $this->assign('color',$color);
+        $this->assign('arr',$arr);
+        $this->assign('site',getSettingCache('site'));
+        $this->assign('back_img',$back_img);
+        $this->assign('recommended',$recommended);
+        return $this->fetch();
+    }
+
 }
