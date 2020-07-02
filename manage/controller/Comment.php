34f9123e79ba42e5dd99e44f7056cdcd7e9e0396
sunhan <sunhan21>
2020-07-02 17:34:27 +0800

    分页/搜索/修改内容

--- manage/controller/Comment.php
+++ manage/controller/Comment.php
@@ -25,7 +25,7 @@ class Comment extends ManageBase
 //        $lists = model('user_comment')->alias('c')->join($join)->field($field)->where($where)->order('c.create_time desc')->paginate(20);
         $join  = [['user u','u.id = f.broker_id'],['second_house s','s.id = f.house_id']];
         $field = 'f.*,u.nick_name,s.title';
-        $lists = model('fydp')->alias('f')->join($join)->field($field)->where($where)->paginate(20);
+        $lists = model('fydp')->alias('f')->join($join)->field($field)->where($where)->paginate(20,false,['query'=>request()->param()]);
 
 
         $this->_exclude = 'edit';
@@ -38,7 +38,7 @@ class Comment extends ManageBase
         $map = [];
 //        $status = input('get.status');
         $admin_id = input('get.admin_id');
-        $map['f.broker_id'] = $admin_id;
+        $map['f.user_id'] = $admin_id;
         $data = [
             'broker_id'  => $admin_id,
         ];
@@ -54,5 +54,26 @@ class Comment extends ManageBase
         \app\common\model\Comment::event('after_delete',function($obj){
         });
         parent::delete();
+    }
+      /**
+     * 修改评论内容
+     */
+    public function beforeEdit()
+    {
+        if($this->request->isPost()){
+            $id = input('post.id');
+            $house_name = input('post.house_name');
+             $arr = model('fydp')->where(array("id"=>$id))->update(['house_name' =>$house_name]);
+             $this->success('保存成功','Comment/index');
+             
+
+        }else{
+            $id = input('param.id/d');
+            $join  = [['user u','u.id = f.broker_id'],['second_house s','s.id = f.house_id']];
+            $field = 'f.*,u.nick_name,s.title';
+            $arr = model('fydp')->alias('f')->join($join)->field($field)->where(array("f.id"=>$id))->find();//7.2sun#分页失效，分页时传递参数
+            $this->assign('arr',$arr);
+            return $this->fetch();
+        }
     }
 }
