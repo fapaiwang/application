6641b7588da53c2f50dbe854c8bcc18796f6a49d
al <>
2020-07-10 17:58:04 +0800

    关注/预约

--- api/controller/User.php
+++ api/controller/User.php
@@ -20,6 +20,13 @@ class User extends Controller
     {
         $this->User_Server = $User_Server;
     }
+
+    /**
+     * 获取关注的房源
+     * @param mixed
+     * @return \think\Paginator|\think\response\Json
+     * @author: al
+     */
     public function followHouse(){
         if (empty(login_user())){
             return $this->error_o("请登录后再操作");
--- home/service/UserService.php
+++ home/service/UserService.php
@@ -15,12 +15,20 @@ class UserService
         $info = \org\Crypt::decrypt($info);
         return $info;
     }
+
+    /**
+     * 关注的房源
+     * @param mixed
+     * @return \think\Paginator
+     * @author: al
+     */
     public function followHouse(){
         $field = "distinct(h.id),h.title,h.estate_name,h.img,h.room,h.living_room,h.toilet,h.price,h.average_price,h.tags,h.address,h.acreage,h.orientations,h.renovation";
-        $lists = $this->getLists('second_house',$field);
+        $lists = $this->getFollowLists('second_house',$field);
         return $lists;
     }
-    private function getLists($model,$field){
+    //关注
+    private function getFollowLists($model,$field){
         $obj = model('follow');
         $where['f.user_id'] = login_user()["id"];
         $where['f.model']   = $model;
@@ -40,4 +48,38 @@ class UserService
             ->paginate(10);
         return $lists;
     }
+
+    /**
+     * 我的预约
+     * @param mixed
+     * @return mixed
+     * @author: al
+     */
+    public function subscribeHouse()
+    {
+        $field = "h.id,h.title,h.estate_name,h.img,h.room,h.living_room,h.toilet,h.price,h.average_price,h.city,h.address,h.acreage,h.orientations,h.renovation";
+        $lists = $this->getSubscribeLists('second_house',$field);
+        $this->assign('lists',$lists);
+        $this->assign('pages',$lists->render());
+        return $this->fetch();
+    }
+
+    private function getSubscribeLists($model,$field)
+    {
+        $obj = model('subscribe');
+        $where['f.user_id'] =login_user()["id"];
+        $where['f.model']   = $model;
+        //$where[] = ['f.house_id','gt',0];
+        $join = [[$model.' h','f.house_id=h.id']];
+        $field .= ',f.create_time,f.type,f.house_id,f.user_name,f.mobile,f.house_name';
+        $lists = $obj->alias('f')
+            ->where($where)
+            ->join($join)
+            ->field($field)
+            ->order('f.create_time','desc')
+            ->group('f.house_id')
+            ->paginate(10);
+        return $lists;
+    }
+
 }
