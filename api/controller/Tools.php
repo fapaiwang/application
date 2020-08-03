7aa3507596c85ba51f6cbf3747fd905644a6cfcd
sunhan <sunhan21>
2020-08-03 15:47:27 +0800

    图片水印6

--- api/controller/Tools.php
+++ api/controller/Tools.php
@@ -99,10 +99,8 @@ class Tools extends Controller
         $num = input('get.num');
         $mulu = scandir('../public/uploads/'.$url.'/');
         unset($mulu[0]);unset($mulu[1]);
-        $accc = array_chunk($mulu, 10);
-        $accc = array(20191204,20191205);
-        //dd($accc);
-        foreach($accc as $k=>$v){
+        $accc = array_chunk($mulu, 5);
+        foreach($accc[$num] as $k=>$v){
             $return[$v]['code'] = 1;
             $data = scandir('../public/uploads/'.$url.'/'.$v.'/');
             foreach($data as $ki=>$vi){
