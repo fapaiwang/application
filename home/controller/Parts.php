ec368e408ee38320fe86fbd73f3c6dbdb285c40d
sunhan <sunhan21>
2020-08-14 17:39:54 +0800

    27

--- home/controller/Parts.php
+++ home/controller/Parts.php
@@ -12,6 +12,12 @@ use think\Controller;
 class Parts extends HomeBase
 {
     public function company_profile(){
+        $seo_s =  model('nav')->field('id,title,url,action,seo_title,seo_keys,seo_desc,model_action')
+            ->where([['status','=',1],['url','=','/parts/company_profile.html']])->find();
+        $seo_s['title'] =$seo_s->seo_title ?? "";
+        $seo_s['keys'] =$seo_s->seo_keys ?? "";
+        $seo_s['desc'] =$seo_s->seo_desc ?? "";
+        $this->assign('seo',$seo_s);
         return $this->fetch();
     }
 
