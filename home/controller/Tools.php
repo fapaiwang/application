96aac7869d5437dbc78560a22db707c33ac34bf2
sunhan <sunhan21>
2020-08-17 13:16:40 +0800

    27

--- home/controller/Tools.php
+++ home/controller/Tools.php
@@ -11,9 +11,9 @@ class Tools extends HomeBase
     }
     //房贷计算
     public function loan(){
-        $seo['title'] = "房贷计算_房拍网贷款工具";
-        $seo['keys']  = "房贷计算";
-        $seo['desc']  = " 提供房贷计算工具，让您清楚房贷需要的金额。";
+        $seo['title'] = "房贷计算器,贷款计算器在线计算,房贷计算工具,税费计算器-金铂顺昌房拍网";
+        $seo['keys']  = "房贷计算器,贷款计算器在线计算,房贷计算工具,税费计算器,金铂顺昌房拍网";
+        $seo['desc']  = "金铂顺昌房拍网为您提供房贷计算器,房贷计算工具,税费计算器,贷款计算器在线计算。金铂顺昌从事法拍房15年，积累千套成交案例。要买便宜房就到金铂顺昌房拍网。";
         $this->assign('seo',$seo);
         return $this->fetch();
     }
@@ -22,25 +22,25 @@ class Tools extends HomeBase
     }
     //税费计算
     public function secondhandtax(){
-        $seo['title'] = " 税费计算_房拍网贷款工具";
-        $seo['keys']  = "税费计算";
-        $seo['desc']  = "提供税费计算，让您明确需要交多少税费。";
+        $seo['title'] = "法拍房,二手房税费计算器,税费计算器-金铂顺昌房拍网";
+        $seo['keys']  = "法拍房,二手房税费计算器,税费计算器,金铂顺昌房拍网";
+        $seo['desc']  = "金铂顺昌房拍网为您提供法拍房,二手房税费计算器,税费计算器在线计算。金铂顺昌从事法拍房15年，积累千套成交案例。要买便宜房就到金铂顺昌房拍网。";
         $this->assign('seo',$seo);
         return $this->fetch();
     }
     //首付成数计算
     public function downpayment(){
-        $seo['title'] = " 首付成数测试_房拍网贷款工具";
-        $seo['keys']  = "首付成数测试";
-        $seo['desc']  = "提供房贷首付成数测试服务。";
+        $seo['title'] = "首付成数测试-金铂顺昌房拍网";
+        $seo['keys']  = "首付成数测试,金铂顺昌房拍网";
+        $seo['desc']  = "金铂顺昌房拍网为您提供法拍房,二手房首付成数测试。金铂顺昌从事法拍房15年，积累千套成交案例。要买便宜房就到金铂顺昌房拍网。";
         $this->assign('seo',$seo);
         return $this->fetch();
     }
     //资质查询
     public function qualification(){
-        $seo['title'] = " 资质查询_房拍网贷款工具";
-        $seo['keys']  = "资质查询";
-        $seo['desc']  = "提供房贷资质查询服务。";
+        $seo['title'] = "购房资质查询工具-金铂顺昌房拍网";
+        $seo['keys']  = "购房资质查询工具,金铂顺昌房拍网";
+        $seo['desc']  = "金铂顺昌房拍网为您提供法拍房,二手房购房资质查询。金铂顺昌从事法拍房15年，积累千套成交案例。要买便宜房就到金铂顺昌房拍网。";
         $this->assign('seo',$seo);
         return $this->fetch();
     }
