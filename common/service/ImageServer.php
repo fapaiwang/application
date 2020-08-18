d44858a0fb92f11d3ff7568d61ee38cef53187b5
sunhan <sunhan21>
2020-08-18 09:36:13 +0800

    27

--- common/service/ImageServer.php
+++ common/service/ImageServer.php
@@ -16,8 +16,8 @@ class ImageServer
         }
         return true;
     }
-    function qiniu_image($key){
-        require'../vendor/qiniu/php-sdk/autoload.php';
+    function QiniuImage($key){
+        include_once'../vendor/qiniu/php-sdk/autoload.php';
         // 用于签名的公钥和私钥
         $accessKey = config('third_party.accessKey');
         $secretKey = config('third_party.secretKey');
@@ -27,14 +27,21 @@ class ImageServer
         // 生成上传 Token
         $token = $auth->uploadToken($bucket);
         // 要上传文件的本地路径
-        $filePath = '../public/'.$key;
+        $filePath = '../public'.$key;
         // 初始化 UploadManager 对象并进行文件的上传。
         $uploadMgr = new UploadManager();
         // 调用 UploadManager 的 putFile 方法进行文件的上传。
+        if($key[0]=='/'){
+            $key = substr($key, 1);
+        }
         list($ret, $err) = $uploadMgr->putFile($token, $key, $filePath);
         if ($err !== null) {
             return $err;
         } else {
+            if(file_exists($key))
+            {
+                unlink($key);
+            }
             return $ret;
         }
     }
